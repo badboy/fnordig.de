9e0f7e41a3c386c25c75cdc522fc58733c623b55
Jan-Erik Rediger <janerik@fnordig.de>
2025-08-31 14:05:24 +0200

    Theme switcher with pure HTML & CSS (and a bit of JS for storage)
    based on https://lyra.horse/blog/2025/08/you-dont-need-js/

--- theme.js
+++ theme.js
@@ -1,18 +1,32 @@
-var toggle = document.getElementById("theme-toggle");
-
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
 if (storedTheme) {
-    document.documentElement.setAttribute('data-theme', storedTheme)
+  document.documentElement.setAttribute('data-theme', storedTheme)
+
+  switch (storedTheme) {
+    case "dark":
+      document.getElementById("theme-dark").checked = true;
+      break;
+    case "light":
+      document.getElementById("theme-light").checked = true;
+      break;
+  }
 }
 
-toggle.onclick = function() {
-  var currentTheme = document.documentElement.getAttribute("data-theme");
-  var targetTheme = "light";
+let radios = document.querySelectorAll("[name=theme-switcher]");
+for (let switcher of radios) {
+  switcher.addEventListener("change", (e) => {
+    let targetTheme = "light"
+    switch (e.target.id) {
+      case "theme-dark":
+        targetTheme = "dark";
+        break;
+      case "theme-light":
+        targetTheme = "light";
+        break;
+    }
 
-  if (currentTheme === "light") {
-    targetTheme = "dark";
-  }
-
-  document.documentElement.setAttribute('data-theme', targetTheme)
-  localStorage.setItem('theme', targetTheme);
-};
+    console.log(`switched to ${targetTheme}`);
+    localStorage.setItem('theme', targetTheme);
+  });
+}
